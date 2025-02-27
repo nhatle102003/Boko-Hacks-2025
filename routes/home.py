9203b6bd8ab7e5b4ad022b6e7eef505d37ceb277
qfc2 <nhle102003@gmail.com>
2025-02-27 16:00:02 -0600

    Implmented OAuth2

--- routes/home.py
+++ routes/home.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, session
+import json
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", session=session.get("user") )
